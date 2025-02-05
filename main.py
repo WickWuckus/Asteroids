a04140124344e280c362ed43b4b779d216b23882
WickWuckus <wickwuckus@gmail.com>
2025-02-04 21:20:17 -0600

    added player movement, and made containers/groups for sprites

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 	dt = 0
 	clock = pygame.time.Clock()
+	updatables = pygame.sprite.Group()
+	drawables = pygame.sprite.Group()
+	Player.containers = (updatables, drawables)
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 	
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
-				return	
-		screen.fill("Black")
+				return
+				
+		screen.fill("black")
+		updatables.update(dt)
+		for drawable in drawables:
+			drawable.draw(screen)
 		pygame.display.flip()
 		dt = clock.tick(60) / 1000
+		
 		
 if __name__== "__main__":
 	main()
